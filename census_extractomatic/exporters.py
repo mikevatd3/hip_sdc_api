806953a35fe929696fcf8aa49ddd53cdc47063e0
JD Godchaux <jd@nijel.org>
2020-04-26 23:19:47 -0500

    typo

--- census_extractomatic/exporters.py
+++ census_extractomatic/exporters.py
@@ -21,7 +21,7 @@ def session(sql_url):
 def get_sql_config(sql_url):
     """Return a tuple of strings: (host, user, password, database)"""
     db_details = urlparse(sql_url)
-    logger.warn('db_details: %s', user.username)
+    logger.warn('db_details: %s', db_details)
     return (db_details.hostname,
             db_details.username,
             db_details.password,
