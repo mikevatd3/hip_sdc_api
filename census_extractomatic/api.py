d3047a45ffeef562f7a0ce0a3e467d79632ca958
JD Godchaux <jd@nijel.org>
2020-11-06 06:17:49 -0600

    testing

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -1008,50 +1008,50 @@ def table_search():
     column_where_parts = []
     column_where_args = {}
 
-    if q and q != '*':
-        q = '%%%s%%' % q
-        table_where_parts.append("lower(tab.table_title) LIKE lower(:query)")
-        table_where_args['query'] = q
-        column_where_parts.append("lower(col.column_title) LIKE lower(:query)")
-        column_where_args['query'] = q
+    # if q and q != '*':
+    #     q = '%%%s%%' % q
+    #     table_where_parts.append("lower(tab.table_title) LIKE lower(:query)")
+    #     table_where_args['query'] = q
+    #     column_where_parts.append("lower(col.column_title) LIKE lower(:query)")
+    #     column_where_args['query'] = q
 
-    if topics:
-        table_where_parts.append('tab.topics @> :topics')
-        table_where_args['topics'] = topics
-        column_where_parts.append('tab.topics @> :topics')
-        column_where_args['topics'] = topics
+    # if topics:
+    #     table_where_parts.append('tab.topics @> :topics')
+    #     table_where_args['topics'] = topics
+    #     column_where_parts.append('tab.topics @> :topics')
+    #     column_where_args['topics'] = topics
 
-    if table_where_parts:
-        table_where = ' AND '.join(table_where_parts)
-        column_where = ' AND '.join(column_where_parts)
-    else:
-        table_where = 'TRUE'
-        column_where = 'TRUE'
+    # if table_where_parts:
+    #     table_where = ' AND '.join(table_where_parts)
+    #     column_where = ' AND '.join(column_where_parts)
+    # else:
+    #     table_where = 'TRUE'
+    #     column_where = 'TRUE'
 
-    # retrieve matching tables.
-    result = db.session.execute(
-        """SELECT tab.tabulation_code,
-                  tab.table_title,
-                  tab.simple_table_title,
-                  tab.universe,
-                  tab.topics,
-                  tab.tables_in_one_yr,
-                  tab.tables_in_three_yr,
-                  tab.tables_in_five_yr
-           FROM census_tabulation_metadata tab
-           WHERE %s
-           ORDER BY tab.weight DESC""" % (table_where),
-        table_where_args
-    )
-    for tabulation in result:
-        tabulation = dict(tabulation)
-        for tables_for_release_col in ('tables_in_one_yr', 'tables_in_three_yr', 'tables_in_five_yr'):
-            if tabulation[tables_for_release_col]:
-                tabulation['table_id'] = tabulation[tables_for_release_col][0]
-            else:
-                continue
-            break
-        data.append(format_table_search_result(tabulation, 'table', ''))
+    # # retrieve matching tables.
+    # result = db.session.execute(
+    #     """SELECT tab.tabulation_code,
+    #               tab.table_title,
+    #               tab.simple_table_title,
+    #               tab.universe,
+    #               tab.topics,
+    #               tab.tables_in_one_yr,
+    #               tab.tables_in_three_yr,
+    #               tab.tables_in_five_yr
+    #        FROM census_tabulation_metadata tab
+    #        WHERE %s
+    #        ORDER BY tab.weight DESC""" % (table_where),
+    #     table_where_args
+    # )
+    # for tabulation in result:
+    #     tabulation = dict(tabulation)
+    #     for tables_for_release_col in ('tables_in_one_yr', 'tables_in_three_yr', 'tables_in_five_yr'):
+    #         if tabulation[tables_for_release_col]:
+    #             tabulation['table_id'] = tabulation[tables_for_release_col][0]
+    #         else:
+    #             continue
+    #         break
+    #     data.append(format_table_search_result(tabulation, 'table', acs))
 
     # retrieve matching columns.
     if q != '*':
