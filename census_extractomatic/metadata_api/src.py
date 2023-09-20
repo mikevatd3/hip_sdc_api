7a01c49625d98fe339950d378645530db6a16c1f
mikevatd3 <mikevatd3@github.com>
2023-09-20 11:29:11 -0400

    Revert "fixed error on map data endpoint"
    This reverts commit 5b68c5036321b211a928a3a6bdebe9c519fc6d90.

--- census_extractomatic/metadata_api/src.py
+++ census_extractomatic/metadata_api/src.py
@@ -9,11 +9,9 @@ metadata_api = Blueprint('metadata_api', __name__)
 def get_db():
     return MetadataSession()
 
-
 @metadata_api.get("/")
 def index():
     return "This is returning stuff."
-
 
 @metadata_api.get("/tables/<tables>")
 def return_table_metadata(tables: str):
