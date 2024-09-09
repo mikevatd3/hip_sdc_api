00ca4e28559f9798dc779893e9728b226ba49cc5
mikevatd3 <mikevatd3@github.com>
2024-09-08 20:51:10 -0400

    turned user_maker into proper cli tool

--- census_extractomatic/auth/user_maker.py
+++ census_extractomatic/auth/user_maker.py
@@ -1,23 +1,36 @@
 from sqlalchemy.orm import Session
 from werkzeug.security import generate_password_hash
+import click
+
 from .models import User
+from ..metadata_api.connection import PublicSession
 
 
 def create_user(
-        username: str, 
-        email: str, 
-        password: str, 
-        pass_confirm: str, 
-        db: Session
-    ) -> None:
+    username: str, email: str, password: str, pass_confirm: str, db: Session
+) -> None:
     if password == pass_confirm:
-        db.add(User(
-            username=username,
-            email=email,
-            password_hash=generate_password_hash(password),
-        ))
+        db.add(
+            User(
+                username=username,
+                email=email,
+                password_hash=generate_password_hash(password),
+            )
+        )
         db.commit()
     else:
         raise ValueError("Passwords didn't match")
 
 
+@click.command()
+@click.argument("username")
+@click.argument("password")
+@click.argument("email")
+@click.argument("pass_confirm")
+def main(username, password, email, pass_confirm):
+    with PublicSession() as db:
+        create_user(username, email, password, pass_confirm, db)
+
+
+if __name__ == "__main__":
+    main()
