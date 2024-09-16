504bc72caf091da8e66341833495d92808ff353a
mikevatd3 <mikevatd3@github.com>
2024-09-16 15:52:34 -0400

    Still mucking through the error types.

--- census_extractomatic/tearsheet.py
+++ census_extractomatic/tearsheet.py
@@ -3,7 +3,7 @@ from urllib.parse import quote, unquote
 from flask import render_template, request, jsonify, Blueprint, current_app
 from flask_cors import CORS, cross_origin
 from sqlalchemy import create_engine, text
-from sqlalchemy.exc import ProgrammingError
+from psycopg2.errors import UndefinedTable
 import tomli
 
 from census_extractomatic._api.download_data import pack_geojson_response
@@ -94,10 +94,10 @@ def sheet():
             )
 
         return jsonify(tearsheet)
-    except (ProgrammingError | Exception) as e:
+    except (UndefinedTable, Exception) as e:
         if how == "html":
             match e:
-                case ProgrammingError():
+                case UndefinedTable():
                     return render_template("error.html", e="The table you're requestiong doesn't exist. Make sure your variables are spelled correctly.")
                 case _:
                     return render_template("error.html", e=e)
